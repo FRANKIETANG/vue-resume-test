1c3378bb515da1aae5ccf02feaf8a6f7d374077d
FRANKIETANG <350558468@qq.com>
2017-09-10 22:09:50 +0800

    数据移动

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -57,48 +57,18 @@ export default {
     name: 'ResumeEditor',
     data() {
         return {
-            selected: 'profile',
-            resume: {
-                config: [
-                    { field: 'profile', icon: 'id' },
-                    { field: 'work history', icon: 'work' },
-                    { field: 'education', icon: 'book' },
-                    { field: 'projects', icon: 'heart' },
-                    { field: 'awards', icon: 'cup' },
-                    { field: 'contacts', icon: 'phone' },
-                ],
-                profile: {
-                    name: '',
-                    city: '',
-                    title: ''
-                },
-                'work history': [
-                    {company: 'AL', content: '我的第二份工作是'},
-                    {company: 'TX', content: '我的第一份工作是'},           
-                ],
-                education: [
-                    {school: 'AL', content: 'xxxxxxxx'},
-                    {school: 'TX', content: 'xxxxxxxx'},                     
-                ],
-                projects: [
-                    {company: 'xxxx', content: 'xxxxxxx'},
-                    {company: 'xxxx', content: 'xxxxxxx'},                     
-                ],
-                awards: [
-                    {company: 'a', content: 'a'},
-                    {company: 'b', content: 'b'},                     
-                ],
-                contacts: [
-                    {company: 'phone', content: '1233445'},
-                    {company: 'qq', content: '654321'},                     
-                ],
-                others: []
-            }
+
         }
     },
     computed: {
         count(){
             return this.$store.state.count
+        },
+        selected(){
+            return this.$store.state.selected
+        },
+        resume(){
+            return this.$store.state.resume
         }
     },
     methods: {
--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,44 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        count: 0
+        count: 0,
+        selected: 'profile',
+        resume: {
+            config: [
+                { field: 'profile', icon: 'id' },
+                { field: 'work history', icon: 'work' },
+                { field: 'education', icon: 'book' },
+                { field: 'projects', icon: 'heart' },
+                { field: 'awards', icon: 'cup' },
+                { field: 'contacts', icon: 'phone' }
+            ],
+            profile: {
+                name: '',
+                city: '',
+                title: ''
+            },
+            'work history': [
+                {company: 'AL', content: '我的第二份工作是'},
+                {company: 'TX', content: '我的第一份工作是'}
+            ],
+            education: [
+                {school: 'AL', content: 'xxxxxxxx'},
+                {school: 'TX', content: 'xxxxxxxx'}
+            ],
+            projects: [
+                {company: 'xxxx', content: 'xxxxxxx'},
+                {company: 'xxxx', content: 'xxxxxxx'}
+            ],
+            awards: [
+                {company: 'a', content: 'a'},
+                {company: 'b', content: 'b'}
+            ],
+            contacts: [
+                {company: 'phone', content: '1233445'},
+                {company: 'qq', content: '654321'}
+            ],
+            others: []
+        }
     },
     mutations: {
         increment (state) {
