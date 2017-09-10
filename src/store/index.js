b3248e69cb93f6ca8d82c0e2ee1b73cd10e61820
FRANKIETANG <350558468@qq.com>
2017-09-11 00:54:13 +0800

    显示简介、工作经历和毕业院校

--- src/store/index.js
+++ src/store/index.js
@@ -9,24 +9,25 @@ export default new Vuex.Store({
         resume: {
             config: [
                 { field: 'profile', icon: 'id' },
-                { field: 'work history', icon: 'work' },
+                { field: 'workHistory', icon: 'work' },
                 { field: 'education', icon: 'book' },
                 { field: 'projects', icon: 'heart' },
                 { field: 'awards', icon: 'cup' },
                 { field: 'contacts', icon: 'phone' }
             ],
             profile: {
-                name: '',
-                city: '',
-                title: ''
+                name: '游戏的王者',
+                city: '木叶村',
+                title: '我要成为海贼王的男人',
+                birthday: '1995-12-30'
             },
-            'work history': [
-                {company: 'AL', content: '我的第二份工作是'},
-                {company: 'TX', content: '我的第一份工作是'}
+            workHistory: [
+                {company: '美国白宫', content: '搞死了记者和自己提拔的州长，怼走了总统，从党鞭当上了总统'},
+                {company: '房车', content: '我有个名字叫 Heisenberg，搞死了所有的竞争对手，New Mexico 最大的毒枭'}
             ],
             education: [
-                {school: 'AL', content: 'xxxxxxxx'},
-                {school: 'TX', content: 'xxxxxxxx'}
+                {school: 'King\'s Landing 的小房子', content: '学了一年跳舞'},
+                {school: 'House of Black and White', content: '学会当一名 no one'}
             ],
             projects: [
                 {company: 'xxxx', content: 'xxxxxxx'},
