05714699de65510bc0f28943f76bafae3437a7eb
FRANKIETANG <350558468@qq.com>
2017-09-10 19:07:39 +0800

    将 visibleItems 改为 config

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -2,9 +2,9 @@
     <div id='resumeEditor'>
         <nav>
             <ol>
-                <li v-for="(item,index) in resume.visibleItems"
-                :class="{active: item === selected}"
-                @click="selected = item"
+                <li v-for="(item,index) in resume.config"
+                :class="{active: item.field === selected}"
+                @click="selected = item.field"
                 >
                 {{index}}
                 </li> 
@@ -18,9 +18,9 @@
             </ol>
         </nav>
         <ol class="panels">
-            <li v-for="item in resume.visibleItems" 
-            v-show="item === selected">
-                 {{resume[item]}}   
+            <li v-for="item in resume.config" 
+            v-show="item.field === selected">
+                 {{resume[item.field]}}   
             </li>
             <!-- <li>1</li>
             <li>2</li>
@@ -38,10 +38,17 @@ export default {
   name: 'ResumeEditor',
   data(){
       return{
-          selected: 'bio',
+          selected: 'profile',
           resume: {
-              visibleItems: ['bio','work history','education','projects','awards','contacts','others'],
-              bio: {
+              config: [
+                  { field: 'profile', icon: 'id' },
+                  { field: 'work history', icon: 'work' },
+                  { field: 'education', icon: 'book' },
+                  { field: 'projects', icon: 'heart' },
+                  { field: 'awards', icon: 'cup' },
+                  { field: 'contacts', icon: 'phone' },
+              ],
+              profile: {
                   name: '',
                   city: '',
                   title: ''
