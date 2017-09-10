dbf4cbf35fa84bb569a59078d15581f053d5afec
FRANKIETANG <350558468@qq.com>
2017-09-10 17:01:04 +0800

    添加导航条+SVG icon

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -1,18 +1,91 @@
 <template>
-  <div id='resumeEditor'>
-      I am resumeEditor
-  </div>
+    <div id='resumeEditor'>
+        <nav>
+            <ol>
+                <li v-for="(item,index) in resume.visibleItems"
+                :class="{active: item === selected}"
+                @click="selected = item"
+                >
+                {{index}}
+                </li> 
+                <!-- <li class="active"> x </li>
+                <li> x </li>
+                <li> x </li>
+                <li> x </li>
+                <li> x </li>
+                <li> x </li>
+                <li> x </li>         -->
+            </ol>
+        </nav>
+        <ol class="panels">
+            <li v-for="item in resume.visibleItems" 
+            v-show="item === selected">
+                 {{resume[item]}}   
+            </li>
+            <!-- <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li> -->
+        </ol>   
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'ResumeEditor'
+  name: 'ResumeEditor',
+  data(){
+      return{
+          selected: 'bio',
+          resume: {
+              visibleItems: ['bio','work history','education','projects','awards','contacts','others'],
+              bio: {
+                  name: '',
+                  city: '',
+                  title: ''
+              },
+              'work history': [],
+              education: [],
+              projects: [],
+              awards: [],
+              contacts: [],
+              others: []
+          }
+      }
+  }
 }
 </script>
 
-<style>
+<style lang='scss'>
   #resumeEditor{
       background: #fff;
       box-shadow: 0 1px 3px 0 rgba(0,0,0,0.25);
+      display: flex;
+      flex-direction: row;
+      overflow: auto;
+      nav{
+          width: 80px;
+          background: black;
+          color: #FFF;
+      }
+      ol{
+          li{
+              height: 48px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              margin-top: 16px;
+              margin-bottom: 16px;
+              &.active{
+                  background: white;
+                  color: black;
+              }
+          }
+      }
+  }
+  ol{
+      list-style: none;
   }
 </style>
