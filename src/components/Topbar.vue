6bf8ffe99e6b05c5469d94d1bc19a03ba3a6c226
FRANKIETANG <350558468@qq.com>
2017-09-11 17:28:18 +0800

    对话框

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -4,16 +4,33 @@
             <span class="logo">Resumer</span>
 
             <div class="actions">
-                <button class="primary">保存</button>
-                <button>预览</button>
+                <a class="button primary" href="#"
+                @click.prevent="signUpDialogVisible = true">注册</a>
+
+                <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible = false">
+                    12345678
+                </MyDialog>
+
+                <a class="button" href="#">登录</a>
+                <button class="button primary">保存</button>
+                <button class="button">预览</button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import MyDialog from './MyDialog'
 export default {
-  name: 'Topbar'
+  name: 'Topbar',
+  data(){
+      return{
+          signUpDialogVisible: false
+      }
+  },
+  components: {
+      MyDialog
+  }
 }
 </script>
 
@@ -39,7 +56,7 @@ export default {
         }
     }
 
-    button{
+    .button{
         width: 72px;
         height: 32px;
         border: none;
@@ -47,6 +64,11 @@ export default {
         font-size: 18px;
         background: #ddd;
         color: #222;
+        text-decoration: none;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
         &:hover{
             box-shadow: 1px 1px 1px hala(0,0,0,0.50);
         }
